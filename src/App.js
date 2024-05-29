181f39c1b3e6861cd6b3ae5e0d8756d4bd17d209
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-05-29 18:48:26 +0200

    Sign up

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import CompareProduct from "./pages/CompareProduct.js";
 import Wishlist from "./pages/Wishlist.js";
 import Login from "./pages/Login.js";
 import Forgotpassword from "./pages/Forgotpassword.js";
+import Signup from "./pages/Signup.js";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
             <Route path="compare-product" element={<CompareProduct />} />
             <Route path="whishlist" element={<Wishlist />} />
             <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
             <Route path="forgot.password" element={<Forgotpassword />} />
           </Route>
         </Routes>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,7 +8,6 @@ const Login = () => {
     <>
       <Meta title={"Login"} />
       <BreadCrumb title="Login" />
-
       <div className="login-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-12">
@@ -32,12 +31,11 @@ const Login = () => {
                   />
                 </div>
                 <div>
-                    <Link to='/forgot.password'>Forgot Password</Link>
-                    <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
-                        <button className="button border-0">Login</button>
-                        <Link className="button signup">SignUp</Link>
-
-                    </div>
+                  <Link to="/forgot.password">Forgot Password</Link>
+                  <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
+                    <button className="button border-0" type="submit">Login</button>
+                    <Link to='/signup' className="button signup">SignUp</Link>
+                  </div>
                 </div>
               </form>
             </div>
